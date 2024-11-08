baf6e937019b25ec468f8d6b4930703a3140d364
XJ303 <157208579+XJ303@users.noreply.github.com>
2024-11-08 20:53:20 +0700

    Create 3.php

---
+++ phiising/3.php
@@ -0,0 +1,34 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Diskominfo News</title>
+    <link rel="icon" type="image/x-icon" href="asset/Logo.png">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+    <link rel="stylesheet" href="index.css">
+
+</head>
+
+<body>
+    <!-- Navigation -->
+    <?php include('includes/header.php'); ?>
+
+    <!-- Full Page Image Header with Vertically Centered Content -->
+    <header class="hero-section">
+        <div class="container d-flex justify-content-between align-items-center text-white">
+            <div class="text-section col-6 mt-5">
+                <h1 class="fw-bolder">Content Media Analysis </h1>
+                <p>Content Media Analysis merupakan website berita karanganyar memberikan analisis tajam untuk membedakan apakah berita tersebut membawa dampak positif atau negatif. Dari inisiatif pembangunan hingga acara budaya,  kami memecahkan setiap informasi menjadi potongan-potongan yang  dapat dimengerti dengan mudah, memberikan Anda wawasan yang lebih dalam tentang realitas sosial dan politik di Karanganyar.</p>    
+            </div>
+            <img class="img-fluid w-25 col-4 mt-5" src="asset/Logo.png" alt="">
+        </div>
+    </header>
+
+    <!-- Page Content -->
+    
+</body>
+
+</html>
